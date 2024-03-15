186d766874c06864167d5cc4790de9cc714ef2c5
unknown <111202214715@mhs.dinus.ac.id>
2024-03-15 10:08:20 +0700

    disable host validation and utilize env file

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 
+	"os"
+
 	"github.com/badoux/checkmail"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 type Email struct {
@@ -28,14 +31,16 @@ func email(c *gin.Context) {
 		return
 	}
 
-	//email domain
-	if err := checkmail.ValidateHost(req.Email); err != nil {
-		fmt.Println(err)
-		c.JSON(200, gin.H{
-			"massage": err.Error(),
-		})
-		return
-	}
+	/*
+		//email domain
+		if err := checkmail.ValidateHost(req.Email); err != nil {
+			fmt.Println(err)
+			c.JSON(200, gin.H{
+				"massage": err.Error(),
+			})
+			return
+		}
+	*/
 
 	c.JSON(200, gin.H{
 		"massage": "valid",
@@ -44,11 +49,17 @@ func email(c *gin.Context) {
 }
 
 func main() {
+	//load env file
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
+
 	router := gin.Default()
 	router.LoadHTMLGlob("template/*")
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.HTML(200, "index.html", nil)
 	})
-	router.POST("/email", email)
-	router.Run(":8080")
+	router.POST(fmt.Sprintf("/%s", os.Getenv("ENDPOINT")), email)
+	router.Run(fmt.Sprintf(":%s", os.Getenv("PORT")))
 }
